bfc7cdf795902467762bd53fbd7ab3835b6cb336
Lana Vu <lanavu@csu.fullerton.edu>
2026-03-24 14:39:33 -0700

    addition_problem_easy: created

---
+++ math_functions.py
@@ -0,0 +1,12 @@
+import random
+
+def addition_problem_easy():
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+    
+    userAnswer = int(input(num1, " + ", num2, " = "))
+
+def addition_checker(userAnswer):
+    
+
+    
