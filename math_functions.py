533390470774b954458048d303a43bdde9bd3e8e
Lana Vu <lanavu@csu.fullerton.edu>
2026-03-24 16:04:40 -0700

    addition_problem: added difficulty parameter for ease

--- math_functions.py
+++ math_functions.py
@@ -1,13 +1,39 @@
 import random
 
-def addition_problem_easy():
-    num1 = random.randint(0, 9)
-    num2 = random.randint(0, 9)
-    
-    userAnswer = int(input(f"{num1} + {num2} = "))
+def addition_problem(difficulty):
+    if difficulty == "Easy":
+        num1 = random.randint(0, 9)
+        num2 = random.randint(0, 9)
+        
+        userAnswer = int(input(f"{num1} + {num2} = "))
 
-    if userAnswer == num1 + num2:
-        print("Correct")
-    else:
-        print("Wrong")
+        if userAnswer == num1 + num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+    if difficulty == "Medium":
+        num1 = random.randint(0, 99)
+        num2 = random.randint(0, 99)
+        
+        userAnswer = int(input(f"{num1} + {num2} = "))
+
+        if userAnswer == num1 + num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+    if difficulty == "Hard":
+        num1 = random.randint(99, 999)
+        num2 = random.randint(999, 999)
+        
+        userAnswer = int(input(f"{num1} + {num2} = "))
+
+        if userAnswer == num1 + num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+def subtraction_problem_easy():
+    num1 = random.randint(0, 9)
     
