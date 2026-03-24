2449df9e71fa469deff5409477149b98d20b3e00
Lana Vu <lanavu@csu.fullerton.edu>
2026-03-24 16:18:04 -0700

    add and sub func altered for more variety, leaving as is rn bc im lazy

--- math_functions.py
+++ math_functions.py
@@ -13,8 +13,13 @@ def addition_problem(difficulty):
             print("Wrong")
 
     if difficulty == "Medium":
-        num1 = random.randint(0, 99)
-        num2 = random.randint(0, 99)
+        choice = random.randint(1, 2)
+        if choice == 1:
+            num1 = random.randint(0, 99)
+            num2 = random.randint(0, 99)
+        if choice == 2:
+            num1 = random.randint(0, 99)
+            num2 = random.randint(0, 999)
         
         userAnswer = int(input(f"{num1} + {num2} = "))
 
@@ -24,9 +29,14 @@ def addition_problem(difficulty):
             print("Wrong")
 
     if difficulty == "Hard":
-        num1 = random.randint(99, 999)
-        num2 = random.randint(999, 999)
-        
+        choice = random.randint(1, 2)
+        if choice == 1:
+            num1 = random.randint(99, 999)
+            num2 = random.randint(100, 999)
+        if choice == 2:
+            num1 = random.randint(100, 999)
+            num2 = random.randint(100, 999)
+
         userAnswer = int(input(f"{num1} + {num2} = "))
 
         if userAnswer == num1 + num2:
@@ -34,6 +44,39 @@ def addition_problem(difficulty):
         else:
             print("Wrong")
 
-def subtraction_problem_easy():
-    num1 = random.randint(0, 9)
+def mul_problem(difficulty):
+    if difficulty == "Easy":
+        num1 = random.randint(0, 9)
+        num2 = random.randint(0, 9)
+        
+        userAnswer = int(input(f"{num1} x {num2} = "))
+
+        if userAnswer == num1 * num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+    if difficulty == "Medium":
+        num1 = random.randint(0, 99)
+        num2 = random.randint(0, 99)
+        
+        userAnswer = int(input(f"{num1} x {num2} = "))
+
+        if userAnswer == num1 * num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+    if difficulty == "Hard":
+        num1 = random.randint(99, 999)
+        num2 = random.randint(999, 999)
+        
+        userAnswer = int(input(f"{num1} x {num2} = "))
+
+        if userAnswer == num1 * num2:
+            print("Correct")
+        else:
+            print("Wrong")
+
+
     
