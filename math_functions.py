6b2d1512d6bd5af5d87748102534bfca7c322a73
Lana Vu <lanavu@csu.fullerton.edu>
2026-03-24 14:43:38 -0700

    addition_problem_easy: f strings to allow input to work correctly, if else to verify instead of adding seperate functioon to verify answers

--- math_functions.py
+++ math_functions.py
@@ -4,9 +4,10 @@ def addition_problem_easy():
     num1 = random.randint(0, 9)
     num2 = random.randint(0, 9)
     
-    userAnswer = int(input(num1, " + ", num2, " = "))
+    userAnswer = int(input(f"{num1} + {num2} = "))
 
-def addition_checker(userAnswer):
-    
-
+    if userAnswer == num1 + num2:
+        print("Correct")
+    else:
+        print("Wrong")
     
