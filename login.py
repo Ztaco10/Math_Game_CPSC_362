7a4cdd6b6bd8e4a63da60e3b28b8165cafbad339
Joshi <aidanjoshi@csu.fullerton.edu>
2026-03-24 21:47:33 -0700

    started building the main menu and welcome message.

--- login.py
+++ login.py
@@ -20,4 +20,24 @@ def print_equals():
     except OSError:
         print(equal * 97)
 
-print_equals()
+def main_menu():
+    print('\033[1m' + 'Main Menu' + '\033[0m')
+    print("1. Login")
+    print("2. Create and Account")
+    print("3. Forgot Password")
+    print("4. About the Game")
+    print("5. Exit Game")
+    choice = int(input("Please enter the corresponding number to where you'd like to go: "))
+    return choice
+
+
+
+
+print('')
+print('\033[1m' + 'HELLO WELCOME TO ZOMBIE OVERFLOW!' + '\033[0m')
+print('')
+choice = 0
+
+while(choice != 5):
+    choice = main_menu()
+    
